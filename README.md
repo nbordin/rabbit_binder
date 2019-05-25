8b18f02d1c65bfd822102507cb9618b08bb99b7f
Nicolò Bordin <nicolofbordin@gmail.com>
2019-05-25 23:22:27 +0200

    fix

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # RabbitMQ binder
 
 This module, provided a .yml file, binds Python functions to RabbitMQ queues.
+
+# Installation
+
+```shell
+pip3 install setuptools && python3 setup.py bdist_wheel
+cd dist
+pip3 install rabbit_binder-0.0.1-py3-none-any.whl
+```
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     description='Python package',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/pypa/sampleproject',
+    url='https://github.com/nbordin/rabbit_binder',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
